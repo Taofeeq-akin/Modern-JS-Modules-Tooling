5d845156576fd9ba0eadded105c55c2d98e132e8
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-29 10:35:47 +0100

    used cloneDeep from loadash packages

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
 // import add from './shoppingCart.js';
 // add('Apple', 10);
 
-
+/*
 // Old way of writing module pattern
 // Module Pattern
 const ShoppingCart2 = (function () {
@@ -50,3 +50,23 @@ const ShoppingCart2 = (function () {
 ShoppingCart2.addToCart('Bread', 5)
 console.log(ShoppingCart2)
 console.log(ShoppingCart2.shoppingCart) // Undefine cus its private
+*/
+
+// To import from loadash
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quality: 5 },
+    { product: 'apples', quality: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+// for deep copy
+const stateClone = Object.assign({}, state)
+const stateDeepClone = cloneDeep(state) // can now use this cus we install loadash already
+
+stateClone.user.loggedIn = false
+console.log(stateClone)
+console.log(stateDeepClone)
