3e7b3a3ae22aa6f0bbc65982dafb15454f75289c
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-29 08:27:53 +0100

    Import all export varaibles at once

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Importing Module
-import { addToCart, cart, totalPrice as price, tq} from './shoppingCart.js'
-console.log('Importing Module')
+// import { addToCart, cart, totalPrice as price, tq} from './shoppingCart.js'
+// console.log('Importing Module')
 
-addToCart('Bread', 5)
-console.log(cart)
+// addToCart('Bread', 5)
+// console.log(cart)
+// console.log(price, tq)
 
-console.log(price, tq)
+// We can also import all module ppt in this way
+import * as ShoppingCart from './shoppingCart.js' // start the import name to make it like an objeact
+
+ShoppingCart.addToCart('Yam', 13)
+console.log(ShoppingCart.totalPrice)
