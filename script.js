a59a9ec4c520b3c6aa7719fcbda14e23ce0f6cbc
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-29 08:50:36 +0100

    Default Export

--- script.js
+++ script.js
@@ -7,7 +7,10 @@
 // console.log(price, tq)
 
 // We can also import all module ppt in this way
-import * as ShoppingCart from './shoppingCart.js' // start the import name to make it like an objeact
+// import * as ShoppingCart from './shoppingCart.js' // start the import name to make it like an objeact
 
-ShoppingCart.addToCart('Yam', 13)
-console.log(ShoppingCart.totalPrice)
+// ShoppingCart.addToCart('Yam', 13)
+// console.log(ShoppingCart.totalPrice)
+
+import add from './shoppingCart.js';
+add('Apple', 10);
