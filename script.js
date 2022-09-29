125d3d0c41e68f1e0e1fb3b8014cdbcbf458a123
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-29 08:14:18 +0100

    Process of Exporting modules

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Importing Module
-import './shoppingCart.js'
+import { addToCart, cart, totalPrice, totalQuantity} from './shoppingCart.js'
 console.log('Importing Module')
+
+addToCart('Bread', 5)
+console.log(cart)
+
+console.log(totalPrice, totalQuantity)
