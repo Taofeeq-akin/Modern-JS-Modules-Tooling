ecdb0ce3aa8ee3a4ad3929f83cc576f63f779373
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-29 08:18:58 +0100

    We can change module varible name either from the expot file or import file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Importing Module
-import { addToCart, cart, totalPrice, totalQuantity} from './shoppingCart.js'
+import { addToCart, cart, totalPrice as price, tq} from './shoppingCart.js'
 console.log('Importing Module')
 
 addToCart('Bread', 5)
 console.log(cart)
 
-console.log(totalPrice, totalQuantity)
+console.log(price, tq)
