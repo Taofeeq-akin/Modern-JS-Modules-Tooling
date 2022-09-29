268b549199194a42358334188dd2104e48a0d456
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-29 16:38:12 +0100

    remove parcel

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Importing Module
 
 // Native ES6 Modeule Pattern
-// import { addToCart, cart, totalPrice as price, tq} from './shoppingCart.js'
+import { addToCart, cart, totalPrice as price, tq} from './shoppingCart.js'
 // console.log('Importing Module')
 
 // import shoppingCart from "./shoppingCart";
 
-// addToCart('Bread', 5)
-// console.log(cart)
-// console.log(price, tq)
+addToCart('Bread', 5)
+console.log(cart)
+console.log(price, tq)
 
 // We can also import all module ppt in this way
-// import * as ShoppingCart from './shoppingCart.js' // start the import name to make it like an objeact
+import * as ShoppingCart from './shoppingCart.js' // start the import name to make it like an objeact
 
-// ShoppingCart.addToCart('Yam', 13)
-// console.log(ShoppingCart.totalPrice)
+ShoppingCart.addToCart('Yam', 13)
+console.log(ShoppingCart.totalPrice)
 
-// import add from './shoppingCart.js';
-// add('Apple', 10);
+import add from './shoppingCart.js';
+add('Apple', 10);
 
 /*
 // Old way of writing module pattern
